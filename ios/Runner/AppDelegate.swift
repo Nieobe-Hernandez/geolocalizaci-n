9009abbe17be6fdb698f7bae4b57a4156b67c34a
Nieobe Hernandez <20223tn057@utez.edu.mx>
2025-11-21 22:43:24 -0600

    [FIRST COMMIT] subida inical del proyecto

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,15 @@
+import Flutter
+import UIKit
+import GoogleMaps   // importa GoogleMaps
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("api_key")   //  tu API Key
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
